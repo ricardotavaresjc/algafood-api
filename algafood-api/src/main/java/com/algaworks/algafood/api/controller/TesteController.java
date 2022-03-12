b21d5ad732a7e166b7cd77477c878055d67ae174
ricardotavaresjc <36016550+ricardotavaresjc@users.noreply.github.com>
2022-03-12 16:55:37 -0300

    arquivos carregados

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -0,0 +1,58 @@
+package com.algaworks.algafood.api.controller;
+
+import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+	@Autowired
+	private CozinhaRepository repository;
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(String nome){
+		return repository.findByNome(nome);
+	}
+
+	@GetMapping("/restaurantes/por-taxa-frete")
+	public List<Restaurante> restaurantesPorTaxaFrete(
+			BigDecimal taxaInicial, BigDecimal taxaFinal){
+		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+	}
+
+	/*
+	 * @GetMapping("/restaurantes/por-nome") public List<Restaurante>
+	 * restaurantesPorNome( String nome, Long cozinhaId){ return
+	 * restauranteRepository.consultarPorNome(nome, cozinhaId); }
+	 */
+
+	@GetMapping("/restaurantes/por-nome-e-frete")
+	public List<Restaurante> restaurantesPorNomeFrete(String nome,
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+		return restauranteRepository.find(nome,taxaFreteInicial,taxaFreteFinal);
+	}
+
+	@GetMapping("/restaurantes/com-frete-gratis")
+	public List<Restaurante> restaurantesComFreteGratis(
+			String nome){
+		return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
+	}
+
+}
