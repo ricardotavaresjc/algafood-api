ce355cfde50f2aaaa3fb8778455c112d9bb1cb34
ricardotavaresjc <36016550+ricardotavaresjc@users.noreply.github.com>
2022-03-12 21:29:22 -0300

    Add files via upload

---
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+
+@Repository
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
+
+	//Busca pela propriedade nome da entidade
+	public List<Cozinha> findByNome(String nome);
+
+	//Busca pela propriedade nome mas com um criterio like
+	public List<Cozinha> findByNomeContaining(String nome);
+
+
+}
