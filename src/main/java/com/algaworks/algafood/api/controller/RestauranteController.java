e4125afaef7e6f8a7977269d10db6bc6a353ed2f
Ricardo Tavares da Silva <ricardotavaresjc@gmail.com>
2022-09-08 23:16:07 -0300

    824 Lançando exception de desserialização na atualização parcial (PATCH)

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,9 +4,15 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletMapping;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +29,7 @@ import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
@@ -74,10 +81,11 @@ public class RestauranteController {
 	}
 
 	@PatchMapping("/{restauranteId}")
-	public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+	public Restaurante atualizarParcial(@PathVariable Long restauranteId, 
+			@RequestBody Map<String, Object> campos, HttpServletRequest request) {
 		Restaurante restauranteAtual = cadastroRestauranteService.buscarOuFalhar(restauranteId);
 
-		merge(campos, restauranteAtual);
+		merge(campos, restauranteAtual, request);
 
 		return atualizar(restauranteId, restauranteAtual);
 	}
@@ -86,37 +94,46 @@ public class RestauranteController {
 	 * Cada propriedade dos dadosOrigem que veio no mapa deve ser passada para
 	 * restauranteDestino usando a API de Reflections do Spring
 	 */
-	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-		/*
-		 * ObjectMapper responsavel por converter(serializar) json em Obj Java ou vice versa
-		 * objectMapper.convertValue(dadosOrigem, Restaurante.class) esta convertendo o que esta vindo
-		 * no corpo para restaurante
-		 */
-		ObjectMapper objectMapper = new ObjectMapper();
-		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
-
-		dadosOrigem.forEach((nomePropriedade, valorPropriedade)-> {
-			/*Declaração do Field do java.lang
-			 * field representa o atributo de restaurante que queremos modificar
-			 * ReflectionUtils é do Spring
-			 * o findField busca na classe Restaurante a propriedade que esta vindo em tempo de execução
-			 * REtorna a instancia de um campo
-			 */
-			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-
-			//Usado para permitir acesso aos metodos privados da classe Restaurante. Torna a variavel privada acessivel
-			field.setAccessible(true);
-
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
+		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
+		try {
+			
 			/*
-			 * retorna o valor de um campo, o valor da propriedade
+			 * ObjectMapper responsavel por converter(serializar) json em Obj Java ou vice versa
+			 * objectMapper.convertValue(dadosOrigem, Restaurante.class) esta convertendo o que esta vindo
+			 * no corpo para restaurante
 			 */
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-
-			/*pega o valor na recuperado na instancia Field field e modifica para o novo valor
-			 *
-			 */
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+			
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade)-> {
+				/*Declaração do Field do java.lang
+				 * field representa o atributo de restaurante que queremos modificar
+				 * ReflectionUtils é do Spring
+				 * o findField busca na classe Restaurante a propriedade que esta vindo em tempo de execução
+				 * REtorna a instancia de um campo
+				 */
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+				
+				//Usado para permitir acesso aos metodos privados da classe Restaurante. Torna a variavel privada acessivel
+				field.setAccessible(true);
+				
+				/*
+				 * retorna o valor de um campo, o valor da propriedade
+				 */
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+				
+				/*pega o valor na recuperado na instancia Field field e modifica para o novo valor
+				 *
+				 */
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+		
 		});
+		}catch(IllegalArgumentException e){
+			Throwable rootCause = ExceptionUtils.getRootCause(e);
+			throw new HttpMessageNotReadableException(e.getMessage(),rootCause, serverHttpRequest);
+		}
 
 
 
