36af25351eee77bd92124d2584fb8a2d595da008
Ricardo Tavares da Silva <ricardotavaresjc@gmail.com>
2022-09-14 06:51:19 -0300

    Modulo 9 Aula 9_4

--- src/main/java/com/algaworks/algafood/api/exceptionhandller/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandller/Problem.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.exceptionhandller;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -11,8 +12,15 @@ public class Problem {
 	private Integer status;
 	private String type;
 	private String title;
-	private String detail;
-	
+	private String detail;	
 	private String userMessage;
 	private LocalDateTime timestamp;
+	private List<Field> fields;
+	
+	@Getter
+	@Builder
+	public static class Field{
+		private String name;
+		private String userMessage;
+	}
 }
