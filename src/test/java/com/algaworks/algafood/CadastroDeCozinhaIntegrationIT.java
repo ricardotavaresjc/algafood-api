bcd5c6f63ceac88e0ea915dbfea75cf73c36141e
Ricardo Tavares da Silva <ricardotavaresjc@gmail.com>
2022-10-11 06:58:40 -0300

    aula 10_9

--- src/test/java/com/algaworks/algafood/CadastroDeCozinhaIntegrationIT.java
+++ src/test/java/com/algaworks/algafood/CadastroDeCozinhaIntegrationIT.java
@@ -15,7 +15,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
 @SpringBootTest
-class CadastroDeCozinhaIntegrationTest {
+class CadastroDeCozinhaIntegrationIT {
 	
 	@Autowired
 	private CadastroCozinhaService service;
