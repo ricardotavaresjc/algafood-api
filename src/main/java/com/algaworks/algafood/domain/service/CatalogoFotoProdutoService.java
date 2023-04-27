f0cb734c113257a43857112f598cdf38f23c429c
Ricardo Tavares da Silva <ricardotavaresjc@gmail.com>
2023-04-27 14:28:19 -0300

    Aula 14_8 e 14_9

--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.domain.service;
 
 
 
+import java.io.InputStream;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
+import com.algaworks.algafood.domain.service.FotoStorageService.NovaFoto;
 
 @Service
 public class CatalogoFotoProdutoService {
@@ -17,10 +19,15 @@ public class CatalogoFotoProdutoService {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private FotoStorageService fotoStorageService;
+	
 	@Transactional
-	public FotoProduto salvar(FotoProduto foto) {
+	public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
 		Long restauranteId = foto.getRestauranteId();
 		Long produtoId = foto.getProduto().getId();
+		String novoNomeArquivo = fotoStorageService.gerarNomeArquivo(foto.getNomeArquivo());
+		
 		//excluir foto se existir
 		Optional<FotoProduto> fotoExistente = produtoRepository
 				.findFotoById(restauranteId, produtoId);
@@ -29,7 +36,20 @@ public class CatalogoFotoProdutoService {
 			produtoRepository.delete(fotoExistente.get());
 		}
 		
-		return produtoRepository.save(foto);
+		foto.setNomeArquivo(novoNomeArquivo);
+		foto =  produtoRepository.save(foto);
+		//Manda o JPA descarregar tudo na memoria no banco
+		produtoRepository.flush();
+		
+		//Nesse caso se der algum erro aqui e lantar uma exceção, ele vai dar rolback no banco
+		NovaFoto novaFoto = NovaFoto.builder()
+				.nomeArquivo(foto.getNomeArquivo())
+				.inputStream(dadosArquivo)
+				.build();
+		
+		fotoStorageService.armazenar(novaFoto);
+		
+		return foto;
 	}
 
 }
---
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/StorageException.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.infrastructure.service.storage;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StorageException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
